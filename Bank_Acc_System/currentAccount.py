a0eed0f5fc4bdd45cbd326e43b0bd855975f29ba
Ajinkya Suroshe <165651188+Ajinkya-00@users.noreply.github.com>
2025-09-18 20:36:25 +0530

    Delete Bank_Acc_System directory

--- Bank_Acc_System/currentAccount.py
+++
@@ -1,5 +0,0 @@
-from account import Account
-
-class CurrentAccount(Account):
-    def __init__(self, acc_no, name, balance = 0):
-        super().__init__(acc_no, name, balance)
--- Bank_Acc_System/savingAccount.py
+++
@@ -1,7 +0,0 @@
-from account import Account
-
-class SavingAccount(Account):
-    def __init__(self, acc_no, name, balance = 0):
-        super().__init__(acc_no, name, balance)
-    
-    
